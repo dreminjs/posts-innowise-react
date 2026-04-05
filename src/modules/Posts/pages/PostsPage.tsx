fdd6bec5c7e49d88c4686dad2a688af86cb62daa
andrew dremin <sankoa230@gmail.com>
2026-04-05 17:16:54 +0300

    feat: availibility delete post from posts page

--- src/modules/Posts/pages/PostsPage.tsx
+++ src/modules/Posts/pages/PostsPage.tsx
@@ -1,9 +1,11 @@
+import { ConfirmDeletionModal } from "../ui/ConfirmDeletion/ConfirmDeletionModal";
 import { Posts } from "../ui/PostsPage/Posts";
 
 export default () => {
   return (
     <>
       <Posts />
+      <ConfirmDeletionModal />
     </>
   );
 };
